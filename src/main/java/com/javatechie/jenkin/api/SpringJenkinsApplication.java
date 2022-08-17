6d10fd37c1e5fb7b9f821ef6e6093d5afbd03c50
vsenku <vsenku@gmail.com>
2022-08-17 18:17:57 +0530

    Fifth Commit

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-@RestController
 public class SpringJenkinsApplication {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
@@ -14,11 +16,6 @@ public class SpringJenkinsApplication {
 	@PostConstruct
 	public void init(){
 		logger.info("Application started...");
-	}
-
-	@GetMapping("/ping")
-	public String message() {
-		return "Wao!! Application Deployed successfully in SAP Cloud..";
 	}
 
 	public static void main(String[] args) {
